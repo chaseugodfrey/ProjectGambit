bf4709fd1769e14e79e73b44333cef530b76f93b
chaseugodfrey <143150195+chaseugodfrey@users.noreply.github.com>
2026-04-06 21:19:34 +0800

    Unity Set up

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float x_MoveSpeed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
