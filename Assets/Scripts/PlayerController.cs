c42fc9bac36d1a58010983bdf3cf2e74819594e7
chaseugodfrey <143150195+chaseugodfrey@users.noreply.github.com>
2026-04-07 19:30:05 +0800

    input actions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,34 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float x_MoveSpeed;
-    
+    public PlayerInput playerInput;
+
+    public Vector2 inputDir;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RegisterInput();
+    }
+
+    void RegisterInput()
+    {
+        InputSystem.actions["Move"].performed += ctx => inputDir = ctx.ReadValue<Vector2>();
+        InputSystem.actions["Move"].canceled += ctx => inputDir = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (InputSystem.actions["Move"].IsPressed())
+            MoveUsingTransform();
+    }
 
+    void UpdateDir(Vector2 dir)
+    {
+        inputDir = dir;
+    }
+    void MoveUsingTransform()
+    {
     }
 }
